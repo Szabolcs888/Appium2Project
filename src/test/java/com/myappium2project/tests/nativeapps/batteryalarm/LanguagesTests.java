7c699644f2f03f3235858580be8a0efccc446750
Szabolcs888 <szigli.szabolcs@gmail.com>
2025-07-10 12:01:17 +0200

    refactor testLanguages: remove Supplier

--- src/test/java/com/myappium2project/tests/nativeapps/batteryalarm/LanguagesTests.java
+++ src/test/java/com/myappium2project/tests/nativeapps/batteryalarm/LanguagesTests.java
@@ -10,7 +10,6 @@ import org.testng.annotations.Test;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.function.Supplier;
 
 @Test(groups = {TestGroups.INTEGRATION})
 public class LanguagesTests extends BatteryAlarmAppTestBase {
@@ -31,42 +30,42 @@ public class LanguagesTests extends BatteryAlarmAppTestBase {
         LanguageUtils.ensureEnglishLanguageSelected(driver, mainPage, languagesDropdownMenu);
 
         // Preserving insertion order is required
-        Map<String, Supplier<String>> expectedTexts = new LinkedHashMap<>();
-        expectedTexts.put("English", () -> "Voice Warning");
-        expectedTexts.put("Czech", () -> "Hlasové varování");
-        expectedTexts.put("Danish", () -> "Stemmeadvarsel");
-        expectedTexts.put("German", () -> "Sprach warnung");
-        expectedTexts.put("Spanish", () -> "Advert. de voz");
-        expectedTexts.put("French", () -> "Avertiss. vocal");
-        expectedTexts.put("Indonesian", () -> "Pering. Suara");
-        expectedTexts.put("Italian", () -> "Avviso vocale");
-        expectedTexts.put("Hungarian", () -> "Hang figyelmeztetés");
-        expectedTexts.put("Dutch", () -> "Gesproken waarsch.");
-        expectedTexts.put("Polish", () -> "Alert głosowe");
-        expectedTexts.put("Portuguese", () -> "Aviso de voz");
-        expectedTexts.put("Romanian", () -> "Avert. vocal");
-        expectedTexts.put("Slovenian", () -> "Hlasové varov.");
-        expectedTexts.put("Swedish", () -> "Röstvarning");
-        expectedTexts.put("Serbian", () -> "Glasovno upozorenje");
-        expectedTexts.put("Finnish", () -> "Ihmisen ääni Varoitus");
-        expectedTexts.put("Turkish", () -> "Sesli uyarı");
-        expectedTexts.put("Bulgarian", () -> "Гласово пред.");
-        expectedTexts.put("Russian", () -> "Голосовое пред.");
-        expectedTexts.put("Ukrainian", () -> "Голосове сповіщ.");
-        expectedTexts.put("Greek", () -> "Φωνητική ειδοποίηση");
-        expectedTexts.put("Vietnamese", () -> "Cảnh báo bằng giọng nói");
-        expectedTexts.put("Japanese", () -> "音声警告");
-        expectedTexts.put("Chinese", () -> "语音警告");
-        expectedTexts.put("Korean", () -> "음성 경고");
-        expectedTexts.put("Thai", () -> "คำเตือนด้วยเสียง");
-        expectedTexts.put("Arabic", () -> "تحذیر بالصوت");
-        expectedTexts.put("Farsi", () -> "هشدار صوتی");
-        expectedTexts.put("Hebrew", () -> "אזהרה קולית");
-        expectedTexts.put("Hindi", () -> "आवाज चेतावनी");
+        Map<String, String> expectedTexts = new LinkedHashMap<>();
+        expectedTexts.put("English", "Voice Warning");
+        expectedTexts.put("Czech", "Hlasové varování");
+        expectedTexts.put("Danish", "Stemmeadvarsel");
+        expectedTexts.put("German", "Sprach warnung");
+        expectedTexts.put("Spanish", "Advert. de voz");
+        expectedTexts.put("French", "Avertiss. vocal");
+        expectedTexts.put("Indonesian", "Pering. Suara");
+        expectedTexts.put("Italian", "Avviso vocale");
+        expectedTexts.put("Hungarian", "Hang figyelmeztetés");
+        expectedTexts.put("Dutch", "Gesproken waarsch.");
+        expectedTexts.put("Polish", "Alert głosowe");
+        expectedTexts.put("Portuguese", "Aviso de voz");
+        expectedTexts.put("Romanian", "Avert. vocal");
+        expectedTexts.put("Slovenian", "Hlasové varov.");
+        expectedTexts.put("Swedish", "Röstvarning");
+        expectedTexts.put("Serbian", "Glasovno upozorenje");
+        expectedTexts.put("Finnish", "Ihmisen ääni Varoitus");
+        expectedTexts.put("Turkish", "Sesli uyarı");
+        expectedTexts.put("Bulgarian", "Гласово пред.");
+        expectedTexts.put("Russian", "Голосовое пред.");
+        expectedTexts.put("Ukrainian", "Голосове сповіщ.");
+        expectedTexts.put("Greek", "Φωνητική ειδοποίηση");
+        expectedTexts.put("Vietnamese", "Cảnh báo bằng giọng nói");
+        expectedTexts.put("Japanese", "音声警告");
+        expectedTexts.put("Chinese", "语音警告");
+        expectedTexts.put("Korean", "음성 경고");
+        expectedTexts.put("Thai", "คำเตือนด้วยเสียง");
+        expectedTexts.put("Arabic", "تحذیر بالصوت");
+        expectedTexts.put("Farsi", "هشدار صوتی");
+        expectedTexts.put("Hebrew", "אזהרה קולית");
+        expectedTexts.put("Hindi", "आवाज चेतावनी");
 
-        for (Map.Entry<String, Supplier<String>> entry : expectedTexts.entrySet()) {
+        for (Map.Entry<String, String> entry : expectedTexts.entrySet()) {
             String language = entry.getKey();
-            String expectedText = entry.getValue().get();
+            String expectedText = entry.getValue();
 
             languagesDropdownMenu.pressLanguageSelectorDropdownMenuButton();
             languagesDropdownMenu.chooseLanguageOption(language);
