746c0f074ed85e52a3edaba1cb458842ee7ba9a6
Szabolcs888 <szigli.szabolcs@gmail.com>
2025-09-16 20:47:53 +0200

    fix: Set correct JDK environment in batch scripts

--- scripts/regression-test.bat
+++ scripts/regression-test.bat
@@ -4,6 +4,10 @@
 cd /d %~dp0..
 echo Running from: %cd%
 
+:: Set runtime environment to Java 22
+set JAVA_HOME=C:\Program Files\Java\jdk-22
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 :: Test run
 mvn test -Dsurefire.suiteXmlFiles=src/test/resources/testng-configs/testng-regression.xml
 
